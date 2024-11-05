fbd694b34a9a622f8d216346bfafb46d50aee626
dowb1004 <hylee97@empal.com>
2024-11-06 05:42:13 +0900

    test

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 ![header](https://capsule-render.vercel.app/api?type=waving&height=300&color=gradient&text=미래지향적인%20프로그래머%20이혜진&fontSize=50)
 
 
@@ -10,6 +11,9 @@
 
 
 [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https://github.com/dowb1004)](https://github.com/dowb1004)                    
+=======
+# scalp
+>>>>>>> 5b5b6ebba32555aec42a2a422322f0126cdd14cf
 =======
 # scalp
 >>>>>>> 5b5b6ebba32555aec42a2a422322f0126cdd14cf
--- pages/intro.py
+++ pages/intro.py
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 import streamlit as st
 
 st.markdown("**🔥 서비스 소개와 안내 사항**")
 st.image("./data/introduction1.png", use_column_width=True)
 st.image("./data/introduction2.png", use_column_width=True)
+=======
+import streamlit as st
+
+st.markdown("**🔥 서비스 소개와 안내 사항**")
+st.image("./data/introduction1.png", use_column_width=True)
+st.image("./data/introduction2.png", use_column_width=True)
+>>>>>>> 5b5b6ebba32555aec42a2a422322f0126cdd14cf
 st.image("./data/introduction3.png", use_column_width=True)
